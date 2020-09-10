450d625d264be33d335950179fc0b6602eb98709
darrickpang <dkypang@yahoo.com>
2020-09-10 16:29:59 -0700

    students done

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :age, :location, :student_dates, :friend_requests_as_receiver, :friend_requests_as_requestor
+  attributes :id, :name, :age, :location, :student_dates, :friend_requests_as_receiver, :friend_requests_as_requestor, :class_sessions
 end
