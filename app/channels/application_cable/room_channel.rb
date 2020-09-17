1d4c5cabb698a2721926129e5e73e16e3c9a3acb
darrickpang <dkypang@yahoo.com>
2020-09-17 13:55:34 -0700

    chat system

--- app/channels/application_cable/room_channel.rb
+++ app/channels/application_cable/room_channel.rb
@@ -4,4 +4,27 @@ class RoomChannel < ApplicationCable::Channel
             stream_from("ChatRoom-#{(params[:room_id])}")
         end
     end
+
+    def speak(data)
+        sender = get_sender(data)
+        room_id = data['room_id']
+        message = data['message']
+
+        conversation = get_conversation(room_id)
+
+        conversation.students << sender unless conversation.students.include?(sender)
+        Message.create!(
+            conversation: conversation,
+            sender: sender,
+            content: message
+        )
+    end
+
+    def get_conversation(room_code)
+        Conversation.find_by(room_code: room_code)
+    end
+
+    def get_sender 
+        Student.find_by(guid: id)
+    end
 end 
